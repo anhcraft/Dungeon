fe1ac7235d05231a250383e1258aaaab80171c3e
anhcraft <huynhduyanh123123@gmail.com>
2022-07-11 12:23:21 +0700

    Updated player appearance

--- entity/PlayerController.gd
+++ entity/PlayerController.gd
@@ -23,10 +23,10 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
-		$Sprite.flip_h = true
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
-		$Sprite.flip_h = false
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_up"):
 		velocity.y = -speed
 	if Input.is_action_pressed("ui_down"):
