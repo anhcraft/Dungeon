9ca6936a683cff36686f7e123ce8294163425739
anhcraft <huynhduyanh123123@gmail.com>
2022-07-13 09:30:48 +0700

    Added basic HUD with stats

--- Main.gd
+++ Main.gd
@@ -5,3 +5,6 @@ func _ready():
 
 func _exit_tree():
 	$World.render_terminated = true
+
+func _process(delta):
+	$HUD/Stats.text = str(Engine.get_frames_per_second()) + " fps | " + str($"/root/Player".position) + " | " + str($World.loaded_chunks.size()) + " loaded chunks"
